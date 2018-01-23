6535eedd604017ca695b073a46e7aac53c8afcef
Gabriel Gularte <gabrielfgularte@gmail.com>
2018-01-23 16:42:15 -0200

    Add deploy improvements

--- deploy.sh
+++ deploy.sh
@@ -1,41 +1,45 @@
 #!/usr/bin/env bash
 USER=ubuntu
 SERVER_URI=example.com
+# SERVER_URI can be an ip (192.168.0.112), an alias located in your .ssh/config file (my-server) or a domain.
 
 git archive -o /tmp/application.tar.gz HEAD:application
-scp /tmp/application.tar.gz $USER@$PROJECT_URI:/tmp/
+tar -czvf /tmp/new_release.tar.gz Dockerfile docker-compose.yml docker-compose.prod.yml nginx.conf
 
-scp Dockerfile $USER@$PROJECT_URI:/tmp/
-scp docker-compose.yml $USER@$PROJECT_URI:/tmp/
-scp docker-compose.prod.yml $USER@$PROJECT_URI:/tmp/docker-compose.override.yml
-scp nginx.conf $USER@$PROJECT_URI:/tmp/nginx.conf
+scp /tmp/application.tar.gz $USER@$SERVER_URI:/tmp/
+scp /tmp/new_release.tar.gz $USER@$SERVER_URI:/tmp/
 
 DATE_NOW=$(date +"%Y%m%d_%H%M%S")
 
-ssh $USER@$PROJECT_URI "
+ssh $USER@$SERVER_URI '
 source ~/.bash_profile
 cd ~/running
 docker-compose down
 cd ~/
-mkdir ~/v-$DATE_NOW
-mv /tmp/Dockerfile ~/v-$DATE_NOW/
-mv /tmp/docker-compose.yml ~/v-$DATE_NOW/
-mv /tmp/docker-compose.override.yml ~/v-$DATE_NOW/
-mv /tmp/nginx.conf ~/v-$DATE_NOW/
-cd ~/v-$DATE_NOW
+mkdir ~/v-'$DATE_NOW'
+cd ~/v-'$DATE_NOW'
+mv /tmp/new_release.tar.gz ~/v-'$DATE_NOW'/
+tar zxvf new_release.tar.gz
+rm new_release.tar.gz
+mv docker-compose.prod.yml docker-compose.override.yml
 mkdir application
 cd application
 tar zxvf /tmp/application.tar.gz
 rm /tmp/application.tar.gz
 cd ~/
-sudo chown -R $USER:$USER ~/running/database
-sudo cp -R ~/running/database ~/v-$DATE_NOW/database
-sudo chown -R $USER:$USER ~/v-$DATE_NOW/database
+test -e ~/running/database/ && \
+sudo chown -R '$USER':'$USER' ~/running/database && \
+sudo cp -R ~/running/database ~/v-'$DATE_NOW'/database && \
+sudo chown -R '$USER':'$USER' ~/v-'$DATE_NOW'/database
 rm -Rf ~/running
-ln -s ~/v-$DATE_NOW/ ~/running
+ln -s ~/v-'$DATE_NOW'/ ~/running
 cd ~/running
 docker-compose up --build --force-recreate -d
 sleep 10
-docker-compose exec django python manage.py migrate
+docker-compose exec -T django ./manage.py migrate
+docker-compose exec -T django ./manage.py collectstatic --noinput
 cd ~/
-"
+LATEST=$(cat ~/latest.txt)
+docker rmi ${LATEST}_django ${LATEST}_django_robot
+echo v'${DATE_NOW//_/}' > ~/latest.txt
+'
