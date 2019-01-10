eaeda86d1fa90df5e7707a33901d43c830cbddc5
Gabriel Gularte <gabrielfgularte@gmail.com>
2019-01-10 15:10:16 -0200

    add media folder to deploy

--- deploy.sh
+++ deploy.sh
@@ -4,10 +4,9 @@ SERVER_URI=example.com
 # SERVER_URI can be an ip (192.168.0.112), an alias located in your .ssh/config file (my-server) or a domain.
 
 git archive -o /tmp/application.tar.gz HEAD:application
-tar -czvf /tmp/new_release.tar.gz Dockerfile docker-compose.yml docker-compose.prod.yml nginx.conf
 
 scp /tmp/application.tar.gz $USER@$SERVER_URI:/tmp/
-scp /tmp/new_release.tar.gz $USER@$SERVER_URI:/tmp/
+scp Dockerfile docker-compose.yml docker-compose.dev.yml nginx.conf $USER@$SERVER_URI:~/
 
 DATE_NOW=$(date +"%Y%m%d_%H%M%S")
 
@@ -17,20 +16,28 @@ cd ~/running
 docker-compose down
 cd ~/
 mkdir ~/v-'$DATE_NOW'
+mv Dockerfile ~/v-'$DATE_NOW'/
+mv docker-compose.yml ~/v-'$DATE_NOW'/
+mv docker-compose.dev.yml ~/v-'$DATE_NOW'/docker-compose.override.yml
+mv nginx.conf ~/v-'$DATE_NOW'/
 cd ~/v-'$DATE_NOW'
-mv /tmp/new_release.tar.gz ~/v-'$DATE_NOW'/
-tar zxvf new_release.tar.gz
-rm new_release.tar.gz
-mv docker-compose.prod.yml docker-compose.override.yml
 mkdir application
 cd application
 tar zxvf /tmp/application.tar.gz
 rm /tmp/application.tar.gz
 cd ~/
+
 test -e ~/running/database/ && \
 sudo chown -R '$USER':'$USER' ~/running/database && \
 sudo cp -R ~/running/database ~/v-'$DATE_NOW'/database && \
 sudo chown -R '$USER':'$USER' ~/v-'$DATE_NOW'/database
+
+test -e ~/running/application/media/ && \
+sudo chown -R '$USER':'$USER' ~/running/application/media && \
+sudo cp -R ~/application/media ~/v-'$DATE_NOW'/application/media && \
+sudo chown -R '$USER':'$USER' ~/v-'$DATE_NOW'/application/media && \
+sudo chmod -R a+wx ~/v-'$DATE_NOW'/application/media
+
 rm -Rf ~/running
 ln -s ~/v-'$DATE_NOW'/ ~/running
 cd ~/running
@@ -40,6 +47,6 @@ docker-compose exec -T django ./manage.py migrate
 docker-compose exec -T django ./manage.py collectstatic --noinput
 cd ~/
 LATEST=$(cat ~/latest.txt)
-docker rmi ${LATEST}_django
-echo v'${DATE_NOW//_/}' > ~/latest.txt
+docker rmi ${LATEST}
+echo v-'${DATE_NOW}'_django > ~/latest.txt
 '
